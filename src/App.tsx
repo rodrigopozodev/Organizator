801a19858a0effe731dc4f6ffda847dcbb7d71bf
rodrigopozodev <rodrigopozosanchez@gmail.com>
2025-11-26 00:30:30 +0100

    Ocultar componente de prueba de alarmas en producciÃ³n

--- src/App.tsx
+++ src/App.tsx
@@ -33,7 +33,7 @@ const AppContent: React.FC = () => {
           ))}
         </div>
 
-        <AlarmTest />
+        {(import.meta.env.MODE === 'development') && <AlarmTest />}
         
         <div className="mt-4 p-4 bg-blue-50 dark:bg-blue-900/20 rounded-lg border border-blue-200 dark:border-blue-800">
           <p className="text-sm text-blue-800 dark:text-blue-200">
